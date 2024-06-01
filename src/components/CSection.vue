3bc494fed048b56762fd7cf16fae6fd86d173131
Croos3r <dorian.moy@epitech.eu>
2024-06-01 11:25:47 +0200

    feat: implemented generic components

---
+++ src/components/CSection.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+export type CSectionProps = {
+	id: string;
+	title: string;
+	number: number;
+};
+
+defineProps<CSectionProps>();
+</script>
+
+<template>
+	<section :id="id" class="my-24">
+		<div class="flex w-full gap-14 max-md:flex-col md:items-center">
+			<h2 class="text-light shrink-0">{{ number > 9 ? number : `0${number}` }}/ {{ title }}</h2>
+			<div class="bg-light h-1 w-full" />
+		</div>
+		<div class="container mx-auto">
+			<slot />
+		</div>
+	</section>
+</template>
+
+<style scoped></style>
