df59ce07b94322f586b972b4ff72bbef6e2d25f5
Croos3r <dorian.moy@epitech.eu>
2024-06-01 11:25:56 +0200

    feat: skeleton of the site

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
+<script lang="ts" setup>
+import CSection from "./components/CSection.vue";
+import CNavigation from "./components/CNavigation.vue";
+</script>
 <template>
-	<h1>Hello World</h1>
+	<header>
+		<CNavigation />
+	</header>
+	<main>
+		<CSection id="about-me" :number="1" title="About me" />
+		<CSection id="competencies" :number="2" title="Competencies" />
+		<CSection id="projects" :number="3" title="Projects" />
+		<CSection id="contact" :number="4" title="Contact" />
+	</main>
+	<footer class="border-t-light mb-5 border-t-4">
+		<CNavigation />
+		<p class="dark:text-light text-dark text-center text-xs">
+			Développé avec <strong class="text-[red]">♥</strong> par <a href="#">moi-même</a><br />
+			Design par <a href="https://fiverr.com/nikolaieva_">Valeriia N</a>
+		</p>
+	</footer>
 </template>
+
+<style>
+body {
+	@apply dark:bg-dark bg-light max-lg:px-[3.125vw] lg:px-[12.5vw];
+}
+</style>
