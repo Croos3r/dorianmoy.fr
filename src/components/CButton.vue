a0fe1ac1e59b28208f4f46fa62adbb76cc8adea8
Croos3r <dorian.moy@epitech.eu>
2024-06-01 11:25:47 +0200

    feat: implemented generic components

---
+++ src/components/CButton.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+export type CButtonProps = {
+	title: string;
+	variant: "fill" | "line";
+};
+
+const { variant } = withDefaults(defineProps<CButtonProps>(), {
+	variant: "fill",
+});
+const dynamicStyle =
+	variant === "fill" ? "bg-primary dark:text-light text-dark" : "border border-primary text-primary  border-2";
+</script>
+
+<template>
+	<button :class="`px-[7.5rem] py-5 ${dynamicStyle}`">
+		{{ title }}
+	</button>
+</template>
+
+<style scoped></style>
---
+++ src/components/CNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+	<nav class="mb-20 mt-12 flex h-14 items-center max-lg:justify-around lg:justify-between">
+		<a class="max-lg:hidden" href="#">
+			<img alt="Dorian's logo" class="h-14 w-auto" src="../assets/logo_alpha.png" />
+		</a>
+		<ul class="dark:text-light text-dark flex gap-x-6 text-sm">
+			<li><a href="#about-me">About me</a></li>
+			<li><a href="#competencies">Competencies</a></li>
+			<li><a href="#projects">Projects</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+	</nav>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped></style>
