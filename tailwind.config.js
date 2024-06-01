a2dc97f702299a1b8dab2d4175efc3390d53715f
Croos3r <dorian.moy@epitech.eu>
2024-06-01 08:58:07 +0200

    feat(theme): configured tailwind theme according to the figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		colors: {
+			dark: "#1b1a21",
+			light: "#f5f5f5",
+			primary: "#c7a763",
+		},
+		fontFamily: {
+			sans: ["Inter", "sans-serif"],
+		},
+		fontSize: {
+			xs: [
+				".875rem",
+				{
+					lineHeight: "0.35rem",
+				},
+			],
+			sm: [
+				"1.125rem",
+				{
+					lineHeight: "0.45rem",
+				},
+			],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
