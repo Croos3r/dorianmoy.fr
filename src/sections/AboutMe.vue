d66c659cf7223d918afbae2b88cb85d26c1c481d
Croos3r <dorian.moy@epitech.eu>
2024-09-08 03:43:55 +0900

    feat: about me section
    Took 27 minutes

---
+++ src/sections/AboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="ml-72 mt-5 flex justify-between text-dark dark:text-light">
+		<p class="max-w-[850px]">
+			Hello, I am Dorian <strong class="text-primary">"Crooser"</strong> Moy, a junior french Fullstack Developer living
+			in Korea 🇰🇷.<br /><br />
+			Currently studying at the <strong class="text-primary">Chung Ang University</strong> in Seoul for my exchange year
+			in Computer Science. In France 🇫🇷, I am a 4th year student at <strong class="text-primary">Epitech</strong> in
+			Paris.<br /><br />
+			I am proficient with <strong class="text-primary">TypeScript-based stacks</strong> but I can work with C/C++, Rust
+			and Java.<br />
+			My year goals are to learn more about <strong class="text-primary">Web3</strong> and
+			<strong class="text-primary">Offensive Cybersecurity</strong> which are two fields that I am passionate about.<br /><br />
+			Lastly, if you cannot find me in front of my computer, I am probably in the nearest EDM festival or club dancing
+			<strong class="text-primary">Hakken</strong>.
+		</p>
+		<img
+			alt="Portrait picture of Dorian Moy"
+			class="h-64 w-64 bg-dark text-light dark:bg-light dark:text-dark"
+			src="../assets/dorianmoy.png"
+		/>
+	</div>
+</template>
+<script></script>
+<style></style>
