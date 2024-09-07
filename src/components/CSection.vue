2d80dbcbce38f219b8acef2c70304a9c5fb70c99
Croos3r <dorian.moy@epitech.eu>
2024-09-08 03:00:57 +0900

    fix(light-theme): some color issues for light theme
    Took 36 minutes

--- src/components/CSection.vue
+++ src/components/CSection.vue
@@ -11,8 +11,8 @@ defineProps<CSectionProps>();
 <template>
 	<section :id="id" class="my-24">
 		<div class="flex w-full gap-14 max-md:flex-col md:items-center">
-			<h2 class="shrink-0 text-light">{{ number > 9 ? number : `0${number}` }}/ {{ title }}</h2>
-			<div class="h-1 w-full bg-light" />
+			<h2 class="shrink-0 text-dark dark:text-light">{{ number > 9 ? number : `0${number}` }}/ {{ title }}</h2>
+			<div class="h-1 w-full bg-dark dark:bg-light" />
 		</div>
 		<div class="container mx-auto">
 			<slot />
