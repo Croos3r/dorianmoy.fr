a27cf14f3e62641c06bc0a893e5200dfb6ccbff0
Croos3r <dorian.moy@epitech.eu>
2024-09-08 03:09:43 +0900

    feat: introduction section
    Took 9 minutes

---
+++ src/sections/Intro.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import CButton from "../components/CButton.vue";
+</script>
+
+<template>
+	<div>
+		<div class="flex flex-col gap-y-2.5 text-dark dark:text-light">
+			<p>Hi, my name is</p>
+			<h1>Dorian Moy</h1>
+			<p class="leading-8">
+				Junior French Fullstack Developer living in Korea<br />
+				Proficient in TypeScript-based stacks<br />
+				Web3 and Cybersecurity enthusiast
+			</p>
+		</div>
+		<a href="#contact">
+			<CButton class="mt-16" title="Get in touch" variant="line" />
+		</a>
+	</div>
+</template>
+
+<style scoped></style>
