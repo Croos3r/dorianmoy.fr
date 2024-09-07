21d2fdf1852e05bc3b7b5db509b9a4cee8e2ba94
Croos3r <dorian.moy@epitech.eu>
2024-09-08 03:00:57 +0900

    fix(light-theme): some color issues for light theme
    Took 36 minutes

--- src/components/CButton.vue
+++ src/components/CButton.vue
@@ -8,11 +8,13 @@ const { variant } = withDefaults(defineProps<CButtonProps>(), {
 	variant: "fill",
 });
 const dynamicStyle =
-	variant === "fill" ? "bg-primary dark:text-light text-dark" : "border border-primary text-primary  border-2";
+	variant === "fill"
+		? "bg-primary dark:text-light text-dark hover:bg-dark hover:text-primary dark:hover:bg-light dark:hover:text-primary"
+		: "border border-primary text-primary border-2 hover:border-dark hover:text-dark dark:hover:border-light dark:hover:text-light";
 </script>
 
 <template>
-	<button :class="`px-[7.5rem] py-5 ${dynamicStyle}`">
+	<button :class="`px-[7.5rem] py-5 transition-all ${dynamicStyle}`">
 		{{ title }}
 	</button>
 </template>
