92ee9a18644d414433c972f13436f590e309973a
Croos3r <dorian.moy@epitech.eu>
2024-09-08 08:38:04 +0900

    feat: competencies section
    Took 1 hour 6 minutes

---
+++ src/components/CCompetency.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+export type CCompetencyProps = {
+	name: string;
+	rating: 1 | 2 | 3 | 4 | 5;
+};
+
+defineProps<CCompetencyProps>();
+</script>
+
+<template>
+	<div class="flex items-center justify-between text-dark dark:text-light">
+		<span>{{ name }}</span>
+		<div class="flex gap-x-1.5">
+			<div v-for="i in Array(rating).fill(0)" :key="i" class="h-2.5 w-2.5 rounded-full bg-primary" />
+		</div>
+	</div>
+</template>
+
+<style scoped></style>
--- src/components/CSection.vue
+++ src/components/CSection.vue
@@ -9,8 +9,8 @@ defineProps<CSectionProps>();
 </script>
 
 <template>
-	<section :id="id" class="my-24">
-		<div class="flex w-full gap-14 max-md:flex-col md:items-center">
+	<section :id="id" class="mb-24 mt-48">
+		<div class="mb-12 flex w-full gap-14 max-md:flex-col md:items-center">
 			<h2 class="shrink-0 text-dark dark:text-light">{{ number > 9 ? number : `0${number}` }}/ {{ title }}</h2>
 			<div class="h-1 w-full bg-dark dark:bg-light" />
 		</div>
