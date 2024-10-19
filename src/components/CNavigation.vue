57300ebf6b91cbebcae0349ee348684cd9144f73
Croos3r <dorian.moy@epitech.eu>
2024-10-19 21:58:50 +0900

    fix(navbar): now hiding navbar on smaller screens
    Took 6 minutes

--- src/components/CNavigation.vue
+++ src/components/CNavigation.vue
@@ -1,10 +1,10 @@
 <template>
 	<nav class="mb-20 mt-12 flex h-14 items-center max-lg:justify-around lg:justify-between">
-		<a class="max-lg:hidden" href="#">
+		<a href="#">
 			<img alt="Dorian's logo" class="hidden h-14 w-auto dark:block" src="../assets/logo_alpha.png" />
 			<img alt="Dorian's logo" class="block h-14 w-auto dark:hidden" src="../assets/logo_alpha_dark.png" />
 		</a>
-		<ul class="flex gap-x-6 text-sm text-dark dark:text-light">
+		<ul class="flex gap-x-6 text-sm text-dark max-sm:hidden dark:text-light">
 			<li><a href="#about-me">About me</a></li>
 			<li><a href="#competencies">Competencies</a></li>
 			<li><a href="#projects">Projects</a></li>
