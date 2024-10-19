b297fa5cac342855e65eeb562804054fea3b2815
Croos3r <dorian.moy@epitech.eu>
2024-10-19 23:07:10 +0900

    fix(projects): moved projects thumbnail pictures to public
    Took 14 minutes
    
    
    Took 1 minute

--- src/components/CProjectThumbnail.vue
+++ src/components/CProjectThumbnail.vue
@@ -17,7 +17,7 @@ withDefaults(defineProps<CProjectThumbnailProps>(), { light: false });
 	<div class="max-md::w-[60vw] md:w-[35vw] lg:w-[22.5vw]">
 		<a :href="url">
 			<div
-				:style="`background-image: url('/src/assets/${thumbnail}')`"
+				:style="`background-image: url('/${thumbnail}')`"
 				class="group relative bg-cover bg-center bg-no-repeat max-md:h-[60vw] md:h-[35vw] lg:h-[22.5vw]"
 			>
 				<FontAwesomeIcon
