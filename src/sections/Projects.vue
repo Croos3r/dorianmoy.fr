5c536ff7fd6c99b1937dfeb833cce0f8b096373c
Croos3r <dorian.moy@epitech.eu>
2024-10-19 21:46:55 +0900

    fix(projects): incoherent value for light project thumbnails, reversed it
    Took 21 seconds

--- src/sections/Projects.vue
+++ src/sections/Projects.vue
@@ -2,23 +2,36 @@
 	<div class="grid justify-items-center gap-y-10 max-md:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 		<CProjectThumbnail
 			description="A simple portfolio for my personal use, the one you're currently browsing."
-			light
 			thumbnail="dorianmoy_fr.png"
 			title="dorianmoy.fr"
 			url="https://dorianmoy.fr"
 		/>
 		<CProjectThumbnail
 			description="The LibertAI website, explaining the project and its goals."
+			light
 			thumbnail="libertai_io.png"
 			title="libertai.io"
 			url="https://libertai.io"
 		/>
 		<CProjectThumbnail
 			description="A confidential version of Notion/Obsidian, on the blockchain. The best of both worlds."
-			light
 			thumbnail="bedrock_im.png"
 			title="bedrock.im"
 			url="https://bedrock.im"
+		/>
+		<CProjectThumbnail
+			description="Mobile and web fullstack application that automates communications and tasks between internet services."
+			light
+			thumbnail="area_github.png"
+			title="Area (School project)"
+			url="https://github.com/RezaRahemtola/Area"
+		/>
+		<CProjectThumbnail
+			description="A small mobile application featuring a customisable dashboard composed of different widgets based on your geolocation."
+			light
+			thumbnail="trombiday_github.png"
+			title="Trombiday (School project)"
+			url="https://github.com/RezaRahemtola/Survivor"
 		/>
 	</div>
 </template>
