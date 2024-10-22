331358c076f2e49ebb69e3102b0db2116e96a459
Croos3r <dorian.moy@epitech.eu>
2024-10-22 12:33:35 +0900

    fix(about-me): changed a bit the paragraph about my languages' skills
    Took 8 minutes

--- src/sections/AboutMe.vue
+++ src/sections/AboutMe.vue
@@ -7,8 +7,8 @@
 			Currently studying at the <strong class="text-primary">Chung Ang University</strong> in Seoul for my exchange year
 			in Computer Science. In France 🇫🇷, I am a 4th year student at <strong class="text-primary">Epitech</strong> in
 			Paris.<br /><br />
-			I am proficient with <strong class="text-primary">TypeScript-based stacks</strong> but I can work with C/C++, Rust
-			and Java.<br />
+			I am mostly proficient with <strong class="text-primary">TypeScript-based stacks</strong> but I can also work with
+			C/C++, Python, Java and Rust.<br />
 			My year goals are to learn more about <strong class="text-primary">Web3</strong> and
 			<strong class="text-primary">Offensive Cybersecurity</strong> which are two fields that I am passionate about.<br /><br />
 			Lastly, if you cannot find me in front of my desk, I am probably in the nearest EDM festival or club dancing
