b6c2c24e0d0e3b5fdc8167e1fa4b5fbbc73fa252
Croos3r <dorian.moy@epitech.eu>
2024-10-25 17:00:15 +0900

    feat(theme-switcher): added button to switch between light and dark themes with transition
    Took 1 hour 14 minutes

---
+++ src/components/CThemeSwitcher.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { ref } from "vue";
+
+const theme = ref<"dark" | "light">(localStorage.theme ?? "light");
+
+function toggleTheme() {
+	theme.value = localStorage.theme = theme.value === "dark" ? "light" : "dark";
+	document.documentElement.classList.toggle("dark");
+}
+</script>
+
+<template>
+	<div
+		:class="theme === 'dark' ? 'bg-primary text-light hover:text-dark' : 'bg-dark text-primary hover:text-light'"
+		class="fixed bottom-5 left-5 h-[5rem] w-[5rem] cursor-pointer rounded-full text-center text-[3rem] leading-[5rem] hover:duration-100"
+		@click="toggleTheme"
+	>
+		<FontAwesomeIcon :icon="theme === 'dark' ? faMoon : faSun" class="transition-all" />
+	</div>
+</template>
+
+<style scoped></style>
