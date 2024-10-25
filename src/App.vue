c6ffb5f16371daff6d687192bb450e4f0c599b8b
Croos3r <dorian.moy@epitech.eu>
2024-10-25 17:00:15 +0900

    feat(theme-switcher): added button to switch between light and dark themes with transition
    Took 1 hour 14 minutes

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,33 @@ import Intro from "./sections/Intro.vue";
 import AboutMe from "./sections/AboutMe.vue";
 import Competencies from "./sections/Competencies.vue";
 import Projects from "./sections/Projects.vue";
+import CThemeSwitcher from "./components/CThemeSwitcher.vue";
+
+if (localStorage.getItem("theme") === null)
+	localStorage.theme =
+		!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+if (localStorage.theme === "dark") document.documentElement.classList.add("dark");
 </script>
 <template>
 	<header>
 		<CNavigation />
 	</header>
 	<main>
-		<Intro class="mb-80 max-md:mt-10 md:mt-44" />
-		<CSection id="about-me" :number="1" title="About me">
+		<Intro class="mb-80 delay-[50ms] max-md:mt-10 md:mt-44" />
+		<CSection id="about-me" :number="1" class="delay-100" title="About me">
 			<AboutMe />
 		</CSection>
-		<CSection id="competencies" :number="2" title="Competencies">
+		<CSection id="competencies" :number="2" class="delay-150" title="Competencies">
 			<Competencies />
 		</CSection>
-		<CSection id="projects" :number="3" title="Projects">
+		<CSection id="projects" :number="3" class="delay-200" title="Projects">
 			<Projects />
 		</CSection>
 	</main>
+	<CThemeSwitcher />
 	<footer class="mb-5 border-t-4 border-t-light">
-		<CNavigation />
-		<p class="text-center text-xs text-dark max-sm:hidden dark:text-light">
+		<CNavigation class="delay-[250ms]" />
+		<p class="text-center text-xs text-dark delay-300 max-sm:hidden dark:text-light">
 			Handcrafted with <strong class="text-[red]">♥</strong> by <a href="#">Me</a><br />
 			Designed by <a href="https://fiverr.com/nikolaieva_">Valeriia N</a>
 		</p>
@@ -33,6 +40,6 @@ import Projects from "./sections/Projects.vue";
 
 <style>
 body {
-	@apply bg-light max-lg:px-[3.125vw] lg:px-[12.5vw] dark:bg-dark;
+	@apply bg-light transition-colors duration-500 max-lg:px-[3.125vw] lg:px-[12.5vw] dark:bg-dark;
 }
 </style>
