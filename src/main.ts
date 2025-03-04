d1f63cbdc04d84699b3fe824a95c8216bf068758
Croos3r <dorian.moy@epitech.eu>
2025-03-04 18:42:15 +0900

    deps: added vuetify to build the contact section
    Took 56 minutes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { inject } from "@vercel/analytics";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+
+// Vue Smooth Scroll
 import VueSmoothScroll from "vue3-smooth-scroll";
 
+// Vercel Analytics
+import { inject } from "@vercel/analytics";
+
+// Vuetify
+import { createVuetify } from "vuetify";
+import { VTextarea } from "vuetify/components/VTextarea";
+import { VTextField } from "vuetify/components/VTextField";
+
+const vuetify = createVuetify({
+	components: {
+		VTextField,
+		VTextarea,
+	},
+	theme: false,
+});
 const app = createApp(App);
-app.use(VueSmoothScroll);
+
+app.use(VueSmoothScroll).use(vuetify);
+
 inject();
+
 app.mount("#app");
