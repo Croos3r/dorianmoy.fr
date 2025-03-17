ab6af2aa091ca70bcd694f86d673c6c15a851e7d
Croos3r <dorian.moy@epitech.eu>
2025-03-17 23:04:39 +0900

    fix: updated competencies
    Took 12 minutes

--- src/sections/Competencies.vue
+++ src/sections/Competencies.vue
@@ -10,7 +10,7 @@ import CCompetencyCategory from "../components/CCompetencyCategory.vue";
 				{ name: 'JS/TS', rating: 4 },
 				{ name: 'Java', rating: 3 },
 				{ name: 'C/C++', rating: 3 },
-				{ name: 'Rust', rating: 1 },
+				{ name: 'Rust', rating: 2 },
 				{ name: 'Haskell', rating: 1 },
 			]"
 			:icon-name="faCode"
@@ -18,9 +18,9 @@ import CCompetencyCategory from "../components/CCompetencyCategory.vue";
 		/>
 		<CCompetencyCategory
 			:competencies="[
-				{ name: 'React', rating: 3 },
+				{ name: 'React', rating: 4 },
 				{ name: 'Vue', rating: 3 },
-				{ name: 'HTML/CSS/JS', rating: 4 },
+				{ name: 'Next', rating: 3 },
 				{ name: 'React-Native', rating: 3 },
 				{ name: 'TailwindCSS', rating: 4 },
 			]"
@@ -29,8 +29,8 @@ import CCompetencyCategory from "../components/CCompetencyCategory.vue";
 		/>
 		<CCompetencyCategory
 			:competencies="[
-				{ name: 'NestJS', rating: 4 },
-				{ name: 'Express', rating: 4 },
+				{ name: 'Nest', rating: 4 },
+				{ name: 'Adonis', rating: 3 },
 				{ name: 'PostgreSQL', rating: 3 },
 				{ name: 'MySQL', rating: 3 },
 				{ name: 'Redis', rating: 3 },
