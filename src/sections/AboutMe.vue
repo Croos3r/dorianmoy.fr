64d3e23af3cfa7d158f8fe810c2be075a7f61ee7
Croos3r <dorian.moy@epitech.eu>
2025-08-07 08:39:50 +0200

    fix: disable vue/no-v-html for static content rendering in footer and about me sections
    Took 6 minutes

--- src/sections/AboutMe.vue
+++ src/sections/AboutMe.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup></script>
 <template>
 	<div class="flex justify-between text-dark 2xl:ml-72 dark:text-light">
-		<p class="max-w-[850px]" v-html="$t('aboutMe.description')">
-		</p>
+		<!-- eslint-disable-next-line vue/no-v-html -->
+		<p class="max-w-[850px]" v-html="$t('aboutMe.description')"></p>
 		<img
 			:alt="$t('aboutMe.altText')"
 			class="h-64 w-64 bg-dark text-light max-md:hidden dark:bg-light dark:text-dark"
