86b4ff9d89d861813ba9e6e5b8e6ddac4c8643aa
Croos3r <dorian.moy@epitech.eu>
2025-08-07 08:33:11 +0200

    feat: added vue-i18n dependency and updated yarn.lock for localization support
    Took 8 seconds

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import VueSmoothScroll from "vue3-smooth-scroll";
+import i18n from "./i18n";
 
 const app = createApp(App);
 app.use(VueSmoothScroll);
+app.use(i18n);
 inject();
 app.mount("#app");
